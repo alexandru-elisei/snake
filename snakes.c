60890ad35d3800891a62bcd4a5ae56941e283033
Alexandru Elisei <alexandru.elisei@gmail.com>
2015-01-09 16:49:10 +0200

    Removed turns array and reworked the way the snake moves

--- snakes.c
+++ snakes.c
@@ -8,13 +8,9 @@
 #include "snakes.h"
 
 static struct Unit *snake;	/* sarpele, duh */
-static struct Unit *turns;	/* punctele de intoarcere */
-/* Directia in care se va misca sarpele la urmatoarea actualizare a ecranului */
-static char next_dir;
-static int snake_len, turns_len;	/* lungimile curente ale sarpelui si ale
-				 * vectorului de puncte de intoarcere */
+static char next_dir;		/* Directia de miscare a sarpelui */
+static int snake_len;		/* lungimea curenta a sarpelui */ 
 static int snake_mem;		/* memoria alocata curent sarpelui */
-static int turns_mem;		/* memoria alocata curent lui turns */
 
 /******************************************************/
 static FILE *f;
@@ -22,14 +18,11 @@ static FILE *f;
 
 
 /* Antet functii locale/private */
-static void aloca_mem(struct Unit **p, int *mem);
-static void adauga_trn(int x, int y, char dir);
-static void del_trn();
+static void aloca_mem(struct Unit **u, int *mem);
 static void creeaza_sarpe();
-static int get_intrn(struct Unit *p);
-static int coord_egale(struct Unit *u1, struct Unit *u2);
 static void muta_unitate(struct Unit *u, char dir);
 static void snk_update();
+static int coord_egale(struct Unit *u1, struct Unit *u2);
 
 /* Aici trebuie sa inceapa sa se miste sarpele */
 void snk_init()
@@ -40,19 +33,13 @@ void snk_init()
 		return;
 	}
 
-	turns_len = 0;
-	turns_mem = 0;
-	aloca_mem(&turns, &turns_mem);
-
-	/* La pornirea jocului sarpele se misca pe orizontala spre dreapta
-	 * incepand cu mijlocul chenarului */
+	/* La pornirea jocului sarpele se misca pe orizontala spre dreapta */
 	creeaza_sarpe();
-
-	/* Sarpele isi va continua miscarea */
-	next_dir = 0;
+	next_dir = RIGHT;
 
 	/* Anunt programul ca incep sa ma joc */
 	flag_add("playing", 1);
+
 	/* Sanity check */
 	flag_del("dead");
 
@@ -70,14 +57,6 @@ void snk_addmv(char dir)
 	else if (dir == UP || dir == DOWN || dir == LEFT || dir == RIGHT)
 		next_dir = dir;
 
-	/******************************************************/
-	/*
-	f = fopen(DEB_FILE, "a");
-	fprintf(f, "snk_addmv -> direction %c\n", next_dir);
-	fclose(f);
-	*/
-	/******************************************************/
-
 	/* Temporary */
 	snk_update();
 }
@@ -85,14 +64,6 @@ void snk_addmv(char dir)
 /* Verifica daca tasta apasata este o directie valida */
 int snk_isdir(char c)
 {
-	/******************************************************/
-	/*
-	f = fopen(DEB_FILE, "a");
-	fprintf(f, "snk_isdir > c = %c\n", c);
-	fclose(f);
-	*/
-	/******************************************************/
-
 	if (c == UP || c == DOWN || c == LEFT || c == RIGHT)
 		return 1;
 
@@ -110,11 +81,6 @@ void snk_reset()
 	snake_mem = 0;
 	free(snake);
 
-	/* Distrug punctele de intoarcere */
-	turns_len = 0;
-	turns_mem = 0;
-	free(turns);
-
 	/* Nu mai ma misc */
 	next_dir = 0;
 }
@@ -122,126 +88,34 @@ void snk_reset()
 /* Am murit */
 void snk_dead()
 {
-	/* Resetez sarpele */
 	snk_reset();
 	flag_add("dead", 1);
 	gph_drwborder();
 	gph_printcenter("** YOU ARE DEAD **");
-
-	/*****************************************************/
-	/*
-	f = fopen(DEB_FILE, "a");
-	fprintf(f, "\nsnk_dead > %d\n", flag_has("dead"));
-	fclose(f);
-	*/
-	/*****************************************************/
 }
 
 /* Genereaza noua pozitie a sarpelui si apoi o deseneaza pe ecran */
 static void snk_update()
 {
-	struct Unit new_snake[snake_len];	/* noua pozitie a sarpelui */
-	int index_trn;
 	int i;
-	FILE *f;
 
-	/* Copiez vechiul sarpe in noul sarpe */
-	for (i = 0; i < snake_len; i ++)
-		new_snake[i] = snake[i];
+	for (i = 1; i < snake_len; i++)
+		snake[i - 1] =  snake[i];
+	muta_unitate(&snake[snake_len - 1], next_dir);
 
-	/***************************************************/
-	/*
-	fprintf(f, "%s", "snk_update - new_snake\n");
-	for (i = 0; i < snake_len; i++)
-		fprintf(f, "new_snake[%d].x = %d, y = %d, dir = %c\n", i,
-				new_snake[i].x, new_snake[i].y, new_snake[i].direction);
-	fprintf(f, "\n");
-	fflush(f);
-	*/
-	/***************************************************/
-
-	if (next_dir != 0)
-		muta_unitate(&new_snake[snake_len - 1], next_dir);
-	else
-		muta_unitate(&new_snake[snake_len - 1], snake[snake_len - 1].direction);
-
-	/* Verific daca urmatoarea pozitie e pe chenar */
-	if (gph_is_onborder(&new_snake[snake_len - 1]) == 1) {
+	/* Verific daca sarpele a intrat in chenar */
+	if (gph_is_onborder(&snake[snake_len - 1]) == 1) {
 		snk_dead();
 		return;
-	}
-
-	
-
-	/* Daca am apasat o tasta si noua directie nu corespunde cu cea veche
-	 * inseamna ca am un punct de intoarcere */
-	if (next_dir != 0 && snake[snake_len - 1].direction != next_dir)
-		adauga_trn(snake[snake_len - 1].x, snake[snake_len - 1].y,
-			       next_dir);
-
-	/* Mut sarpele */
-	for (i = snake_len - 2; i >= 0; i--) {
-		/* Daca un segment al sarpelui se afla pe un punct de 
-		 * intoarcere */
-		index_trn = get_intrn(&snake[i]);
-		if (index_trn != -1)
-			next_dir = turns[index_trn].direction;
-		/* Daca nu se afla, nu isi va schimba directia */
-		else
-			next_dir = snake[i].direction;	
-
-		/* Mut segmentul curent al sarpelui */
-		muta_unitate(&new_snake[i], next_dir);	
 	}
 
 	/* Verific daca sarpele nu intra in coliziune cu el insusi */
-	if (snk_is_incolision(&new_snake[snake_len - 1], new_snake) == 1) {
+	if (snk_is_incolision(&snake[snake_len - 1], snake) == 1) {
 		snk_dead();
 		return;
-	}	
+	}
 
-	/***************************************************/
-	/*
-	fprintf(f, "%s", "snk_update - dupa ce am mutat sarpele new_snake\n");
-	for (i = 0; i < snake_len; i++)
-		fprintf(f, "new_snake[%d].x = %d, y = %d, dir = %c\n", i,
-				new_snake[i].x, new_snake[i].y, new_snake[i].direction);
-	fprintf(f, "\n");
-	fflush(f);
-	*/
-	/***************************************************/
-
-	/* Sterg primul punct de intoarcere, noul sarpe deja a trecut de el
-	 * complet */
-	if (turns_len > 0 && snake[0].x == turns[0].x && snake[0].y == turns[0].y )
-		del_trn();
-
-	/* Copiez noul sarpe in cel vechi */
-	for (i = 0; i < snake_len; i++)
-		snake[i] = new_snake[i];
-
-	/* Desenez sarpele pe ecran */
 	gph_drwsnk(snake, snake_len);
-
-	/* Astept input de la user ca sa schimb directia curenta sarpelui */
-	next_dir = 0;
-
-	//fclose(f);
-}
-
-/* Verifica daca o unitate a sarpelui este punct de intoarcere */
-static int get_intrn(struct Unit *p)
-{
-	int i;
-
-	if (turns_len == 0)
-		return -1;
-
-	for (i = 0; i < turns_len; i++)
-		if (p->x == turns[i].x && p->y == turns[i].y)
-			return i;
-
-	return -1;
 }
 
 /* Verifica daca o unitate nu apartine sarpelui, pentru a detecta coliziunile*/
@@ -262,19 +136,6 @@ int snk_is_incolision(struct Unit *u,		/* pointer catre un element */
 		return 1;
 	else
 		return 0;
-}
-
-/* Adauga un punct de intoarcere, adica un punct unde sarpele isi schimba
- * directia de miscare */
-static void adauga_trn(int x, int y, char dir)
-{
-	if (turns_len == turns_mem)
-		aloca_mem(&turns, &turns_mem);
-
-	turns[turns_len].x = x;
-	turns[turns_len].y = y;
-	turns[turns_len].direction = dir;
-	++turns_len;
 }
 
 /* Aloca memorie pentru vectori de tip Unit */
@@ -308,23 +169,12 @@ static void creeaza_sarpe()
 
 	gph_getcenter(&x, &y);
 	x -= 2;
+
 	for (i = 0; i < snake_len; i++) {
 		snake[i].x = x + i;
 		snake[i].y = y;
 		snake[i].direction = RIGHT;
 	}
-
-	/***********************************************************/
-	/*
-	f = fopen(DEB_FILE, "a");
-	fprintf(f, "%s", "creeaza_sarpe -> sarpele initial:\n");
-	for (i = 0; i < snake_len; i++)
-		fprintf(f, "snake[%d].x = %d, y = %d, dir = %c\n", i,
-				snake[i].x, snake[i].y, snake[i].direction);
-	fflush(f);
-	fclose(f);
-	*/
-	/***********************************************************/
 }
 
 /* Muta un punct in directia indicata */
@@ -346,17 +196,6 @@ static void muta_unitate(struct Unit *u, char dir)
 	}
 
 	u->direction = dir;
-}
-
-/* Sterge primul element al vectorului de puncte de intoarcere */
-static void del_trn()
-{
-	int i;
-
-	for (i = 1; i < turns_len; i++)
-		turns[i - 1] = turns[i];
-
-	--turns_len;
 }
 
 /* Verifica daca doua unitati, trimise ca pointer, au aceleasi coordonate */
