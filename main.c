a167cf0b6d16f7c56e8df3910d968de7e9f53798
Alexandru Elisei <alexandru.elisei@gmail.com>
2015-01-11 13:40:13 +0200

    Snake changes speed depending on level

--- main.c
+++ main.c
@@ -11,7 +11,13 @@
 #define SELECT_ERROR	-1	/* fatal error */
 
 #define VI_SEC		0	/* viteza initiala, in secunda */
-#define VI_USEC		600000	/* viteza initiala, in microsecunde */
+#define VI_USEC		500000	/* viteza initiala, in microsecunde */
+
+#define VINC_SEC	0	/* cu cat creste viteza de afisaj pe nivel */
+#define VINC_USEC	100000	/* cu cat creste viteza de afisaj pe nivel */
+
+#define VMAX_SEC	0	/* viteza de afisaj maxima (in secunde) */
+#define VMAX_USEC	100000	/* viteza de afisaj maxima (in microsecunde) */
 
 /* Antet functii */
 
@@ -89,13 +95,25 @@ int main(void)
 			if (snk_isdir(key) == 1)
 				snk_addmv(key);
 
-			FD_SET(0, &read_descriptors);
-
-			viteza.tv_sec = VI_SEC;
-			viteza.tv_usec = VI_USEC;
-
 			snk_move();
 			error_check("DRAWING SNAKE (Not enough memory?)");
+
+			if (flag_has("lvlup") != 0) {
+				fprintf(f, "next level, yay!\n");
+				flag_del("lvlup");
+			}
+
+			FD_SET(0, &read_descriptors);
+
+			viteza.tv_sec = VI_SEC - score_lvl() * VINC_SEC;
+			viteza.tv_usec = VI_USEC - score_lvl() * VINC_USEC;
+
+			if (viteza.tv_sec < VMAX_SEC)
+				viteza.tv_sec = VMAX_SEC;
+
+			if (viteza.tv_usec < VMAX_USEC)
+				viteza.tv_usec = VMAX_USEC;
+				
 		} else if (flag_has("menu_mode") != 0) {
 			key = gph_getkey();
 
